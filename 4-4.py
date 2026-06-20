4650a0212678ac98df8a5015aad9a083c59b839d
김민관 <rbimvakf@gmail.com>
2026-06-20 22:06:25 +0900

    Create 4-4.py

---
+++ 4-4.py
@@ -0,0 +1,10 @@
+print("you" "need" "python")
+print("you"+"need"+"python")
+print("you", "need", "python")
+print("".join(["you", "need", "python"]))
+'''
+youneedpython
+youneedpython
+you need python
+youneedpython
+'''
