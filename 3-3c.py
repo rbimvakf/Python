0f31db579bf83036246464f28b1eb5ca36421322
김민관 <rbimvakf@gmail.com>
2026-06-20 21:12:56 +0900

    Create 3-3c.py

---
+++ 3-3c.py
@@ -0,0 +1,22 @@
+i = 0
+while True:
+    i += 1
+    if(i > 5):
+        break
+    print("*" * i)
+while True:
+    i -= 1
+    if(i < 2):
+        break
+    print("*" * (i - 1))
+'''
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+'''
