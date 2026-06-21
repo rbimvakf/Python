7285dcbb94c2038a45e5fbcc3cc7d25b55484594
김민관 <rbimvakf@gmail.com>
2026-06-21 17:36:14 +0900

    Create 5-12.py

---
+++ 5-12.py
@@ -0,0 +1,16 @@
+import random
+
+numbers = [i for i in range(1, 46)]
+
+def lottery(data):
+    number = random.choice(data)
+    data.remove(number)
+    return number
+
+result = [lottery(numbers) for i in range(6)]
+result.sort()
+
+print("당첨번호: ", end="")
+
+for i in range(6):
+    print("%d " % result[i], end="")
