0217a487783a0c44fe26392572164e1e51b3ba2e
김민관 <rbimvakf@gmail.com>
2026-06-21 16:43:37 +0900

    Update 4-5.py

--- 4-5.py
+++ 4-5.py
@@ -1,7 +1,7 @@
-f = open("C:/Users/SHHERKY/Desktop/Magic.txt", 'w')
+f = open("Magic.txt", 'w')
 f.write("Life is too short")
 f.close()
 
-f = open("C:/Users/SHHERKY/Desktop/Magic.txt", 'r')
+f = open("Magic.txt", 'r')
 print(f.read())
 f.close()
